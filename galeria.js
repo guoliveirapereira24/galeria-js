6e36ad810dad08abd7c4dcec5899a5711cf34435
GUSTAVO DE OLIVEIRA PEREIRA <21193137@CFP127ALUNO.LOCAL>
2021-10-06 10:27:11 -0300

    Galeria de cachorros

--- galeria.js
+++ galeria.js
@@ -1,41 +1,87 @@
 "use strict"
 
-const imagens = [
-    "./img/Cris.jpg",
-    "./img/Liz.jpg",
-    "./img/balu2.png",
-    "./img/Arthur2.jpg",
-    "./img/Thiago.jpg",
-    "./img/Erin2.png",
-    "./img/joui5.jpg",
-    "./img/Kaiser3.jpg",
-    "./img/Dante2.jpg",
-    "./img/Beatrice.jpg",
-    "./img/Alex.jpg"
-]
+const limparElemento = (elemento) => {
+    while(elemento.firstChild){
+        elemento.removeChild(elemento.lastChild)
+    }
+}
+
+
+const pegarImagens = (raca) => fetch(`https://dog.ceo/api/breed/${raca}/images`)
+
+const pesquisarImagens = async (evento) =>{
+    if(evento.key === "Enter") {   
+    const raca = evento.target.value         
+    const imagensResponse = await pegarImagens(raca)
+    const imagens = await imagensResponse.json(raca)
+    
+
+    limparElemento (document.querySelector(".galeria-container"))
+    limparElemento (document.querySelector(".slide-container"))
+
+    carregarGaleria(imagens.message)
+    carregarSlide(imagens.message)
+    }
+}
 
 // MAP - FILTER - REDUCE -> Declarativa
 
+const limparId = (url) =>  {
+    const ultimaBarra = url.lastIndexOf("/") + 1
+    const ultimoPonto = url.lastIndexOf(".")
+    return console.log(url.substring(ultimaBarra, ultimoPonto).replace(" ", ))
+}
+
 const criarItem = (urlImagem) => {
- /*   const container = document.querySelector(".galeria-container")
+    const container = document.querySelector(".galeria-container")
     
     const novoLink = document.createElement("a")
-    novoLink.href = urlImagem 
+    novoLink.href = `#${limparId(urlImagem)}` 
     novoLink.classList.add("galeria-items")
     novoLink.innerHTML = `<img src="${urlImagem}" alt=""></img>`
     
     container.appendChild(novoLink)
-
-
+    
+    
     /*container.innerHTML += `
-        <a href="${urlImagem}" class="galeria-items">
-            <img src="${urlImagem}" alt="">
-        </a>
+    <a href="${urlImagem}" class="galeria-items">
+    <img src="${urlImagem}" alt="">
+    </a>
     `
     */
 }
 
 
-const carregarImagens = () => imagens.forEach(criarItem)
 
-carregarImagens()
+
+const carregarGaleria = (imgs) => imgs.forEach(criarItem)
+
+const criarSlide = (urlImagem, indice, arr) => {
+    const container = document.querySelector(".slide-container")
+    const novoDiv = document.createElement("div")
+    novoDiv.classList.add("slide")
+    novoDiv.id = limparId(urlImagem)
+
+
+    const indiceAnterior = indice <= 0 ? arr.length -1 : indice - 1;
+    const idAnterior = limparId(arr[indiceAnterior])
+
+    const indiceProximo = indice >= arr.length -1 ? 0 : indice + 1
+    const idProximo = limparId(arr[indiceProximo])
+
+
+    novoDiv.innerHTML = `
+        <div class="imagem-container">
+            <a href="#" class="icones fechar">&#10006;</a>
+            <a href="${idAnterior}" class="icones anterior">&#171;</a>
+            <img src="${urlImagem}" alt="">
+            <a href="${idProximo}" class="icones proximo">&#187;</a>
+        </div>
+    `
+    container.appendChild(novoDiv)
+}
+
+const carregarSlide = (imgs) => imgs.forEach(criarSlide)
+
+document.querySelector(".pesquisa-container input")
+        .addEventListener("keypress", pesquisarImagens)
